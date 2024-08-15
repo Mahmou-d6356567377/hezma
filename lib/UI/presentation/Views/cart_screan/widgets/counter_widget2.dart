ebf1070daaa855dbc68d80b4b03ba418d925e5f0
Mahmou-d6356567377 <131625133+Mahmou-d6356567377@users.noreply.github.com>
2024-08-15 15:19:26 +0300

    take the value from the count of item and make total price

--- lib/UI/presentation/Views/cart_screan/widgets/counter_widget2.dart
+++ lib/UI/presentation/Views/cart_screan/widgets/counter_widget2.dart
@@ -3,8 +3,13 @@ import 'package:hezma/utils/constants.dart';
 import 'package:hezma/utils/fonts.dart';
 
 class CounterWidget2 extends StatefulWidget {
+  final int initialCount;
+  final ValueChanged<int> onCountChanged;
+
   const CounterWidget2({
     super.key,
+    required this.initialCount,
+    required this.onCountChanged,
   });
 
   @override
@@ -12,26 +17,37 @@ class CounterWidget2 extends StatefulWidget {
 }
 
 class _CounterWidgetState extends State<CounterWidget2> {
-  int count = 1;
+  late int count;
+
+  @override
+  void initState() {
+    super.initState();
+    count = widget.initialCount;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         Container(
-            decoration: BoxDecoration(
-                color: const Color(backgroundcustomgreen2),
-                borderRadius: BorderRadius.circular(20)),
-            child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    count++;
-                  });
-                },
-                child: const Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ))),
+          decoration: BoxDecoration(
+            color: const Color(backgroundcustomgreen2),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                count++;
+                widget.onCountChanged(count); 
+              });
+            },
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
+        ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 12.0),
           child: Text(
@@ -40,19 +56,25 @@ class _CounterWidgetState extends State<CounterWidget2> {
           ),
         ),
         Container(
-            decoration: BoxDecoration(
-                color: const Color(backgroundcustomgreen2),
-                borderRadius: BorderRadius.circular(20)),
-            child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    count--;
-                  });
-                },
-                child: const Icon(
-                  Icons.remove,
-                  color: Colors.white,
-                ))),
+          decoration: BoxDecoration(
+            color: const Color(backgroundcustomgreen2),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                if (count > 1) {
+                  count--;
+                  widget.onCountChanged(count); 
+                }
+              });
+            },
+            child: const Icon(
+              Icons.remove,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ],
     );
   }
