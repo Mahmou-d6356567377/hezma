4b715e4a53a5857645e0d39319a3317ed41d5df7
Mahmou-d6356567377 <131625133+Mahmou-d6356567377@users.noreply.github.com>
2024-08-15 15:22:57 +0300

    refactor Item screan3

---
+++ lib/UI/presentation/Views/Item_screan/widgets/price_details_container.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:hezma/UI/presentation/Views/Item_screan/widgets/total_price_widget.dart';
+import 'package:hezma/utils/constants.dart';
+import 'package:hezma/utils/fonts.dart';
+
+class PriceDetailsContainer extends StatelessWidget {
+  const PriceDetailsContainer({
+    super.key,
+    required this.totalprice,
+  });
+
+  final int totalprice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 65,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(1), // Shadow color
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+        color: const Color(backgroundcolor1),
+        borderRadius: BorderRadius.circular(17),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                    actions: <Widget>[
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 20),
+                          child: Image.asset(kcarticon),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'تمت اضافة المنتج الى السله',
+                              style: arabicstyle2,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                    surfaceTintColor: const Color(backgroundcolor1),
+                    shadowColor: Colors.black26,
+                  ),
+                );
+              },
+              child: Stack(
+                children: [
+                  Image.asset(krecaddcart),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 15),
+                    child: Image.asset(kaddcart),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Spacer(),
+          TotalPriceWidget(totalprice: totalprice),
+        ],
+      ),
+    );
+  }
+}
+
